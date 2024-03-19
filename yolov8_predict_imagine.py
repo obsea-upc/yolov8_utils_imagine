a5613f360e72185257e3bf450362ee576527e59f
PolBanosCastello <polbc98@gmail.com>
2024-03-19 15:17:29 +0100

    uograde prediction file to introduce video inference, but is not aviable yet

--- yolov8_predict_imagine.py
+++ yolov8_predict_imagine.py
@@ -1,14 +1,19 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, settings
 import os
 from rich import print
 from tqdm import tqdm
 
 # Load a pretrained YOLOv8n model
-model = YOLO('./weigths/18sp_2301img_xlarge_lr0_000375_1920_workers6.pt')
+model = YOLO('./weigths/19sp_2538img_17425annots_xlarge_lr0_000375_1280_seed_1714_workers16_autobatch.pt')
 
 path = '/home/polba/sarti/datasets/temp_train/images/'
 im_list = [path + im for im in os.listdir(path)]
-
+print(settings)
 # new OBSEA imgsz=2688 × 1520
 for path in tqdm(im_list):
-    model.predict(path, save=True, save_txt=True, imgsz=2688, conf=0.5, line_width=2)
+    model.predict(path, save=True, save_txt=True, imgsz=1920, conf=0.5, line_width=2)
+    # model.predict(path, save=True, save_txt=True, imgsz=2688, conf=0.5, line_width=2)
+
+# ## video infeerence
+# path = '/home/polba/sarti/datasets/Videos_ML/instagram_1_original.mp4'
+# model.predict(path, save=True, save_txt=True, imgsz=1920, conf=0.5, line_width=2)
